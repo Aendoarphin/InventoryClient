dbff291d6415b05c0b9d4308812fcdc5ed298da1
Arhon Pineda <apineda@wsfcu.com>
2025-10-02 17:04:22 -0500

    remove component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 import "../App.css";
 import Home from "@/components/Home";
-import NeedsAttention from "@/components/NeedsAttention";
 
 export const Route = createFileRoute("/")({
   component: App,
@@ -15,7 +14,6 @@ function App() {
         className="h-max mx-auto grid grid-cols-2 gap-4 *:bg-card *:shadow-md xl:max-w-8/12 xl:mx-auto"
       >
         <Home />
-        <NeedsAttention />
       </div>
     </div>
   );
