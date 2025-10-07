e3ff88f6759213dcd1b6288a7e16e50d8faba9c0
Arhon Pineda <apineda@wsfcu.com>
2025-10-07 17:19:21 -0500

    add chartjs

--- src/components/NeedsAttention.tsx
+++ src/components/NeedsAttention.tsx
@@ -1,46 +1,21 @@
-import { IconInfoCircle } from "@tabler/icons-react";
+import useMetrics from "@/hooks/useMetrics";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { useState } from "react";
+import { Doughnut } from "react-chartjs-2";
 
-const attentionItems = [
-  {
-    category: "Empty values in 'Items'",
-    description: "Some fields are missing values; address this issue or mark as complete if those values are not needed.",
-  },
-  {
-    category: "Empty values in 'Vendors'",
-    description: "Some fields are missing values; address this issue or mark as complete if those values are not needed.",
-  },
-];
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const warning = "#F2C03F";
+const good = "#31B531";
 
 function NeedsAttention() {
+  const metrics: Record<string, any> = useMetrics();
+  console.log(metrics[0])
+  console.log(metrics[1])
+  const [labels] = useState<string[]>(['Complete', 'Needs Attention'])
   return (
     <div className="col-span-2 p-4 border border-muted shadow-md">
-      <h2 className="flex justify-between items-center">
-        Needs Attention{" "}
-        <IconInfoCircle title="The following items must be addressed to maintain data integrity" />
-      </h2>
-      <div id="attention-items-container" className="border border-muted flex flex-column gap-2 max-h-96 overflow-y-scroll">
-        {attentionItems.length > 0 ? (
-          <table className="**:p-4 w-full">
-            <thead className="border-b border-muted sticky top-0 bg-thead">
-              <tr className="*:text-start">
-                <th scope="col">Category</th>
-                <th scope="col">Description</th>
-              </tr>
-            </thead>
-            <tbody className="last:border-0">
-              {attentionItems.map((item, index) => (
-                <tr key={index} className="odd:bg-neutral-200">
-                  <td scope="row">{item.category}</td>
-                  <td>{item.description}</td>
-                </tr>
-              ))}
-              {}
-            </tbody>
-          </table>
-        ) : (
-          <h6 className="w-full text-muted text-center py-6">No Items</h6>
-        )}
-      </div>
+      {/* <Doughnut data={null}></Doughnut> */}
     </div>
   );
 }
---
+++ src/hooks/useMetrics.ts
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+
+function useMetrics() {
+  const [metrics, setMetrics] = useState<Record<string, any>[]>([])
+
+  useEffect(() => {
+    async function fetchMetrics() {
+      const itemMetrics = await axios.get(`https://${import.meta.env.VITE_WEBAPI_IP}:7097/api/Item/metrics`)
+      console.log(itemMetrics);
+      const vendorMetrics = await axios.get(`https://${import.meta.env.VITE_WEBAPI_IP}:7097/api/Vendor/metrics`)
+      console.log(vendorMetrics);
+      setMetrics([itemMetrics.data, vendorMetrics.data])
+    }
+    fetchMetrics();
+  }, []);
+
+  return metrics
+}
+
+export default useMetrics
