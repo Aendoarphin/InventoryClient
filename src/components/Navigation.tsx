131a3d597bcf543dfd594cd3b5a601387d5f746c
Arhon Pineda <apineda@wsfcu.com>
2025-10-31 16:14:39 -0500

    add semantic header

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,13 +3,15 @@ import NavLinks from "./NavLinks";
 
 function Navigation() {
   return (
-    <nav className="p-4 bg-primary sticky top-0 shadow-sm flex flex-row justify-between z-40 text-white">
-      <Link to="/" className="text-shadow-lg">
-        <h2 className="inline">{import.meta.env.VITE_COMPANY_NAME}</h2>&nbsp;&nbsp;
-        <h5 className="underline inline">Inventory Manager</h5>
-      </Link>
-      <NavLinks />
-    </nav>
+    <header>
+      <nav className="p-4 bg-primary sticky top-0 shadow-sm flex flex-row justify-between z-40 text-white">
+        <Link to="/" className="text-shadow-lg">
+          <h2 className="inline">{import.meta.env.VITE_COMPANY_NAME}</h2>&nbsp;&nbsp;
+          <h5 className="underline inline">Inventory Manager</h5>
+        </Link>
+        <NavLinks />
+      </nav>
+    </header>
   );
 }
 
