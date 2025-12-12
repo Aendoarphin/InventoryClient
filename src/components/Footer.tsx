56198d6e5c4e576c59d5b44b1f0404f6f363b879
Arhon Pineda <apineda@wsfcu.com>
2025-12-12 14:16:04 -0600

    include version number

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import { baseApiUrl } from "@/static";
+import { baseApiUrl, versionNumber } from "@/static";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
@@ -22,11 +22,13 @@ function Footer() {
   }, []);
 
   return (
-    <footer className="border-t border-muted fixed bottom-0 left-0 right-0 h-max flex justify-between px-4 bg-card">
+    <footer className="border-t border-muted fixed bottom-0 left-0 right-0 h-max flex justify-between px-4 bg-card text-muted">
       <div>
-        @ {new Date().getFullYear()} {import.meta.env.VITE_COMPANY_NAME} Inventory Manager
+        <p>@ {new Date().getFullYear()} {import.meta.env.VITE_COMPANY_NAME} Inventory Manager</p>
       </div>
-      <div>
+      <div className="inline-flex gap-2">
+        <p>Version: {versionNumber}</p>
+        |
         <p>API Status: {status}</p>
       </div>
     </footer>
